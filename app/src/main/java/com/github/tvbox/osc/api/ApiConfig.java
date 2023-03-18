6d83712a5c53b3175288cd858837c9ff761b5101
hbbfly <128218114+hbbfly@users.noreply.github.com>
2023-03-18 17:25:19 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https//www.345676.xyz/tvbox仓库.txt");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https//www.345676.xyz/tvbox仓库.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
